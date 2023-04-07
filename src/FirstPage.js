dc4a1c20444a6ab8dc32d76b62c2d4af6da6d32d
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-04-08 00:11:38 +0300

    ilk sayfa tamamlandı.

---
+++ src/FirstPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./FirstPage.css";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import MainPage from "./MainPage";
+export default function FirstPage() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <div className="firstPageContainer">
+            <div className="heading">
+              <h1>YEMEK DÜNYASI</h1>
+              <h5>BİR DİLİM LEZZET BİNLERCE MUTLULUK :)</h5>
+              <Link to="/orderpizza">
+                <button>MUTLU OL!</button>
+              </Link>
+            </div>
+            <img src="https://www.eauclairesbestpizza.com/wp-content/uploads/2022/06/pizza-6-speciality-1-1fx9ae.png" />
+          </div>
+        </Route>
+        <Route exact path="/orderpizza">
+          <MainPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
